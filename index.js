f93ebfe6d09f1200ee837c9ffd678a30018095cf
eyeland <63192606+eyeland@users.noreply.github.com>
2022-03-03 20:14:49 -0500

    add player collision and end game if so

--- index.js
+++ index.js
@@ -110,9 +110,9 @@ function spawnEnemies() {
     }, 1000)
 }
 
-
+let animationId 
 function animate() {
-    requestAnimationFrame(animate)
+    animationId = requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.forEach(projectile => {
@@ -121,6 +121,12 @@ function animate() {
 
     enemies.forEach((enemy, index) => {
         enemy.update()
+
+        //player hit detection ** END GAME **
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+        if(dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId)
+        }
 
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
